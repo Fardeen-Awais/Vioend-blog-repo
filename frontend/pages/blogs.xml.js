4b0724226a3dde4a5d9696fd168366b2f58905cf
fardeen <fardeenjutt99@gmail.com>
2022-12-10 08:33:01 -0800

    Fix issue in blog sitemap

--- frontend/pages/blogs.xml.js
+++ frontend/pages/blogs.xml.js
@@ -4,12 +4,6 @@ function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>https://jsonplaceholder.typicode.com</loc>
-     </url>
-     <url>
-       <loc>https://jsonplaceholder.typicode.com/guide</loc>
-     </url>
      ${posts
        .map((arg) => {
          return `
